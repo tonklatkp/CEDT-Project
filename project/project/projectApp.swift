532fd3e4eca6eccc61adb26fbffb06edccbd631e
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:22:14 -0700

    first commit

---
+++ project/project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by admin on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
