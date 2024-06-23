636ece96555bf31f5204091d77d0f9b42de3555c
admin <tonklaforwork1@gmail.com>
2024-06-22 21:07:24 -0700

    2ui

---
+++ project/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  project
+//
+//  Created by admin on 6/22/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
